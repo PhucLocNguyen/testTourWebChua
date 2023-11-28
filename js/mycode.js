0197a0c754d3eeb51ac99404624748e618b7af76
PhucLocNguyen <loc19992003bca@gmail.com>
2023-11-28 23:58:44 +0700

    Second Update

--- js/mycode.js
+++ js/mycode.js
@@ -1,17 +1,18 @@
 function CalTotal(event){
     event.preventDefault();
    var constValue = document.querySelector("#formSection .floatingBox .show").getAttribute("data");
-   var output = constValue* event.target[1].value;
-   document.querySelector("#total").innerHTML=`<p class="text-center" id="total">Total Amount : ${output}$</p>`;
+    var output = constValue* event.target[1].value;
+        document.querySelector("#total").innerHTML=`<p class="text-center" id="total">Total Amount : ${output}$</p>`;
 }
 
 function getConstValue(event){
     var constValue=event.target.value;
     switch(constValue){
+        case "0": realValue =0;
         case "1": realValue =150;
         break;
         case "2": realValue = 250;
         break;
     }
-    document.querySelector("#formSection .floatingBox").innerHTML = `<p class ="show" data ="${realValue}">${realValue}$/Person</p>  `;
+    document.querySelector("#formSection .floatingBox").innerHTML = `<p class ="show" data ="${realValue}">${realValue}$/Person</p>  `
 }
